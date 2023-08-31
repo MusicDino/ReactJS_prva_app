90d2afd82ec8f12cc70cb256853e7e42a35a6436
Dino Musić <dmusic996@gmail.com>
2023-08-31 20:21:20 +0200

    verzija1

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <b>Moja <code>prva</code> aplikacija</b>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://hjp.znanje.hr/index.php?show=search_by_id&id=dlpmXxA%3D"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Nauči reagirati
         </a>
       </header>
     </div>
